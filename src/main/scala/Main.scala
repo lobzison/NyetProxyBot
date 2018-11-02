c7ec778589018755ed666666038c7d81eddff8e9
Gleb <lobzison@gmail.com>
2018-11-02 20:30:20 +0100

    removed shotdown

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,10 +14,6 @@ object Main extends App {
     case (Some(t), Some(o)) => {
       val bot = Bot(t,o.toLong)
       val eol = bot.run()
-      println("Press [ENTER] to shutdown the bot, it may take a few seconds...")
-      scala.io.StdIn.readLine()
-      bot.shutdown() // initiate shutdown
-      // Wait for the bot end-of-life
       Await.result(eol, Duration.Inf) // ScalaJs wont't let you do this
     }
   }
